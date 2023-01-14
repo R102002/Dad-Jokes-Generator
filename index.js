b6e01315fdeb8575b858b96655490277bcae15f5
R102002 <riddhiradia014@gmail.com>
2023-01-14 09:35:22 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const btnEl = document.getElementById("btn");
+const jokeEl = document.getElementById("joke");
+const apiKey = "9dJr3LtCudc+F6icQEOHhg==j3rUO0VcBUwH033n";
+const options = {
+  method: "GET",
+  headers: {
+    "X-Api-Key": apiKey,
+  },
+};
+
+const apiURL = "https://api.api-ninjas.com/v1/dadjokes?limit=1";
+async function getJoke() {
+  try {
+    jokeEl.innerText = "Updating...";
+    btnEl.disabled = true;
+    btnEl.innerText = "Loading...";
+    const response = await fetch(apiURL, options);
+    const data = await response.json();
+    btnEl.disabled = false;
+    btnEl.innerText = "Tell me a joke";
+    jokeEl.innerText = data[0].joke;
+  } catch (error) {
+    jokeEl.innerText = "An error happened,try again later";
+    btnEl.disabled = false;
+    btnEl.innerText = "Tell me a joke";
+    console.log(error);
+  }
+}
+btnEl.addEventListener("click", getJoke);
